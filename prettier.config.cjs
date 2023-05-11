8220e0f8a625f14845d547eb35c089c5cd8810fc
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-05-11 11:05:10 +0200

    add prettier plugin for tailwind

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+// prettier.config.js
+module.exports = {
+  plugins: [require("prettier-plugin-tailwindcss")],
+  // auto-detected: tailwindConfig: './tailwind.config.js',
+  singleQuote: false,
+  trailingComma: "es5",
+  requirePragma: false,
+  arrowParens: "always",
+};
