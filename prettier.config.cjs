ca5d48873b0a4a524426ca1bc5c7037014bd0e12
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-05-11 11:06:50 +0200

    update prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,8 +2,8 @@
 module.exports = {
   plugins: [require("prettier-plugin-tailwindcss")],
   // auto-detected: tailwindConfig: './tailwind.config.js',
-  singleQuote: false,
-  trailingComma: "es5",
+  // default: trailingComma: "es5",
+  singleQuote: true,
   requirePragma: false,
   arrowParens: "always",
 };
