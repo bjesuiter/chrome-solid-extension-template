32727c60429a77839b344c86a189f10a3a35fb18
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-12-28 16:20:04 +0100

    bun add -D tailwindcss postcss autoprefixer

--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -4,27 +4,16 @@ import styles from "./App.module.css";
 
 export function App() {
   return (
-    <div class="fixed bottom-5 left-5 top-20 z-[2000] w-80 rounded-xl bg-white">
-      {/* <div class={styles.App}> */}
-      <header class={styles.header}>
-        <img
-          width={200}
-          src={chrome.runtime.getURL(logo)}
-          class={styles.logo}
-          alt="logo"
-        />
-        <p class="flex flex-wrap font-bold">
-          Edit <code>src/pages/content/index.tsx</code> and save to reload.
-        </p>
-        <a
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-      {/* </div> */}
+    <div class=" fixed left-5 top-5 z-[2000] w-80 rounded-xl bg-[#282c34] text-white">
+      <img
+        width={200}
+        src={chrome.runtime.getURL(logo)}
+        class={styles.logo}
+        alt="logo"
+      />
+      <p class="flex flex-wrap font-bold ">
+        Edit <code>src/pages/content/index.tsx</code> and save to reload.
+      </p>
     </div>
   );
 }
