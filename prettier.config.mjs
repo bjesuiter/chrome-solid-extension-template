bae3b42b731cdb929dcb687732627d86b339b94a
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-01-22 22:37:49 +0100

    set single quotes back to false to match previous code style

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,14 +1,14 @@
 // prettier.config.mjs
 
-import * as prettierPluginTailwind from 'prettier-plugin-tailwindcss';
+import * as prettierPluginTailwind from "prettier-plugin-tailwindcss";
 
 const config = {
   // prettierPluginTailwind auto-detects: tailwindConfig: './tailwind.config.js',
   plugins: [prettierPluginTailwind],
   // default: trailingComma: "es5",
-  singleQuote: true,
+  singleQuote: false,
   requirePragma: false,
-  arrowParens: 'always',
+  arrowParens: "always",
 };
 
 export default config;
