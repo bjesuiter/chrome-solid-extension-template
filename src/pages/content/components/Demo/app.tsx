00b43a8a8d81e9b55ccf730a5d578f022ff7159a
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-04-25 09:31:23 +0200

    remove legacy scss file

--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -3,7 +3,7 @@ import "@src/styles/tailwind.css";
 
 export function App() {
   return (
-    <div class="fixed top-5 left-5 z-[2000] w-80 rounded-xl bg-[#282c34] text-white">
+    <div class="fixed top-5 left-5 z-[2000] w-120 rounded-xl bg-[#282c34] text-white">
       <img
         src={logoDataUrl}
         class="motion-preset-spin motion-duration-[2s] pointer-events-none"
